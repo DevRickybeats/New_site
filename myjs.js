8fade8893666ee2cfcac0187ed01d0bafd07da13
IGWE RICKSON IKENNA <rickybeats24@gmail.com>
2023-01-15 13:37:38 +0000

    js

--- myjs.js
+++ myjs.js
@@ -1,9 +1,4 @@
 
-// This code will create a function that will display an alert message when the page is loaded.
-// window.onload = function () {
-//     alert("Ricky Dev!");
-// };
-
 
 document.querySelector('ul').style.position = 'absolute';
 document.querySelector('ul').style.bottom = '0';
