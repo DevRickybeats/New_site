275e9a7175deed36ab994614bd1c38d76b6d5393
IGWE RICKSON IKENNA <rickybeats24@gmail.com>
2023-01-20 13:48:19 +0000

    update

--- myjs.js
+++ myjs.js
@@ -1,5 +1,11 @@
 
-
 document.querySelector('ul').style.position = 'absolute';
 document.querySelector('ul').style.bottom = '0';
 
+// document.getElementById("paragraph-container").addEventListener("mouseover", function () {
+//     this.style.transform = "rotateX(0deg) rotateY(0deg)";
+// });
+
+// document.getElementById("paragraph-container").addEventListener("mouseout", function () {
+//     this.style.transform = "rotateX(50deg) rotateY(50deg)";
+// });
